abd78d1337ddc2ef31d061b191c1d08178bbdeda
Christopher Paquin <cpaquin966@gmail.com>
2024-12-17 10:58:54 -0700

    open modules to framework

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,4 +9,8 @@ module paqattack.pirateslife {
 
     opens paqattack.pirateslife to javafx.fxml;
     exports paqattack.pirateslife;
+    opens paqattack.pirateslife.windows to javafx.fxml;
+    exports paqattack.pirateslife.windows;
+    opens paqattack.pirateslife.util to javafx.fxml;
+    exports paqattack.pirateslife.util;
 }
